cd1a3fdbada5623484f81c7f1059542f918b6407
Enis Budancamanak <enis.budancamanak@hotmail.com>
2022-08-26 19:16:24 +0200

    undo

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -7,32 +7,28 @@
 	import Popup from '../components/Popup.svelte';
 	import LoadingScreen from '../components/Loadings/PatternLoading.svelte';
 	import { gltfLamp } from '../store/stores';
-	// import { useGltf } from 'threlte/extras';
-	// const { gltf } = useGltf('3DModels/Wagenfeldlampe/wagenfeldlampe_24.glb');
+	import { useGltf } from 'threlte/extras';
+	const { gltf } = useGltf('3DModels/Wagenfeldlampe/wagenfeldlampe_24.glb');
 	import { page } from '$app/stores';
 
 	let timerLoading = true; //default: true
 	let t1;
 
-	// $: if ($gltf) {
-	// 	gltfLamp.set($gltf);
+	$: if ($gltf) {
+		gltfLamp.set($gltf);
 
-	// 	setTimeout(function () {
-	// 		t1.play();
-	// 	}, 0);
-	// }
+		setTimeout(function () {
+			t1.play();
+		}, 0);
+	}
 
 	onMount(async () => {});
 
 	onMount(() => {
-		fetch('3DModels/Wagenfeldlampe/wagenfeldlampe_24.glb').then((test) => {
-			console.log(test);
-		});
-
 		// setTimeout(function () {
+		// 	t1.play();
 		// }, 0);
 		t1 = gsap.timeline({ paused: true, defaults: { duration: 0.4 } });
-		t1.play();
 
 		const loadingScreen = document.getElementById('quoteLoading');
 
@@ -46,7 +42,6 @@
 </script>
 
 <div id="content" style="margin: 0 auto;">
-	<a href="3DModels/Wagenfeldlampe/wagenfeldlampe_24.glb" download> file_name </a>
 	<div class="flex flex-col min-h-screen">
 		{#if !timerLoading}
 			<Nav />
